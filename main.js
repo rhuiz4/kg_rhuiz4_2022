ecdb8547953550f55cf0f172898f38a938857da1
Rihui Zheng <rz1276@nyu.edu>
2021-05-03 16:02:29 -0400

    Added testing and parsing input

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
-var phonetics = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
-var result = []
-
+const PHONETICS = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+var ARGS = process.argv;
 
 function convert(array){
-    result = []
+    /*
+    Converts an integer array into its phonetic representation
+    Params: Array<int> of integer values
+    Return: String with the phonetic string of the integers in the array
+    */
+    let result = []
     for (let i = 0; i < array.length; i++){
-        let curr_num = array[i]
+        let curr_num = array[i];
         if (curr_num == 0){
-            result.push("Zero")
+            result.push("Zero");
         }
         else {
-            let curr_str = ""
+            let curr_str = "";
             while (curr_num) {
-                curr_dig = curr_num%10
-                curr_num = Math.floor(curr_num/10)
-                curr_str = (phonetics[curr_dig]) + curr_str
+                curr_dig = curr_num%10;
+                curr_num = Math.floor(curr_num/10);
+                curr_str = (PHONETICS[curr_dig]) + curr_str;
             }
-            result.push(curr_str)
+            result.push(curr_str);
         }
     }
+    return result.join(",");
 }
 
-let a = [314,143215425,5135209,51531]
-convert(a)
+function main(){
+    console.log(convert(ARGS.slice(2)))
+}
+main();
 
-console.log(result.join(","))
+module.exports = { convert };   // For the test file
